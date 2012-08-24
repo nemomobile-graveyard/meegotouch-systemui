f0cd7ee4e963254bacf22e6f461cca12fd23ceca
Robin Burchell <robin+git@viroteck.net>
2012-08-24 14:19:58 +0200

    BMC 17258: profile dialog replaced by settings profile applet

--- demos/plugins/profile/profile.cpp
+++ demos/plugins/profile/profile.cpp
@@ -22,6 +22,7 @@
 #include <MButton>
 #include <MButtonGroup>
 #include <QGraphicsLinearLayout>
+#include <QDBusInterface>
 #include <MLayout>
 #include <MLinearLayoutPolicy>
 #include <MDialog>
@@ -43,42 +44,12 @@ Profile::~Profile()
 
 void Profile::showProfileDialog()
 {
-    // Create a dialog for choosing the profile
-    MDialog* dialog = new MDialog("Profile", M::NoStandardButton);
-    MWidget *centralWidget = new MWidget;
-    QGraphicsLinearLayout *layout = new QGraphicsLinearLayout(Qt::Vertical);
-    centralWidget->setLayout(layout);
-    dialog->setCentralWidget(centralWidget);
 
-    // Create a button group for the profiles
-    MButtonGroup *buttonGroup = new MButtonGroup(centralWidget);
-    buttonGroup->connect(buttonGroup, SIGNAL(buttonClicked(MButton *)), dialog, SLOT(accept()));
-
-    // Place the buttons in the button group and in the layout
-    MButton *button;
-    button = new MButton("Ringing");
-    button->setCheckable(true);
-    button->setChecked(true);
-    layout->addItem(button);
-    buttonGroup->addButton(button);
-    button = new MButton("Silent");
-    button->setCheckable(true);
-    layout->addItem(button);
-    buttonGroup->addButton(button);
-    button = new MButton("Beep");
-    button->setCheckable(true);
-    layout->addItem(button);
-    buttonGroup->addButton(button);
-    button = new MButton("Loud");
-    button->setCheckable(true);
-    layout->addItem(button);
-    buttonGroup->addButton(button);
-
-    // Show the dialog
-    dialog->exec();
-
-    // Hide the status indicator menu
-    if(MStatusIndicatorMenuInterface *menu = plugin->statusIndicatorMenuInterface()) {
+  QDBusInterface launcher ("com.nokia.DuiControlPanel", "/",
+                                  "com.nokia.DuiControlPanelIf");
+  launcher.call ("appletPage", "Profile");
+  // Hide the status indicator menu
+  if(MStatusIndicatorMenuInterface *menu = plugin->statusIndicatorMenuInterface()) {
         menu->hideStatusIndicatorMenu();
-    }
+  }
 }
